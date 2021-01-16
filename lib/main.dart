b508f2fb8451a0588454ab876eeecc6c78b4c57f
[Vershima kelvin] <[kelvinityavyar@gmail.com]>
2021-01-16 21:58:43 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int x = 1;
+  int y = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  x = Random().nextInt(6) + 1;
+                  y = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$y.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  y = Random().nextInt(6) + 1;
+                  x = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dice$x.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
